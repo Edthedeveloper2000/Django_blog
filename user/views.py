431c84302b6c05a7ded648a406f540fc59f86c2b
Edthedeveloper2000 <edmarcosmendes2019@gmail.com>
2020-07-29 13:14:21 -0300

    enviando arquivos inicias

---
+++ user/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegistration
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistration(request.POST)
+        print("test 1")
+        if form.is_valid():
+            form.save()
+            print("test 2")
+            username = form.cleaned_data.get('username')
+            messages.success(request,f'your account has been created, now you\'re able to log in ')
+            return redirect('login')
+    else:   
+        form = UserRegistration()
+    return render(request,'user/register.html',{'form': form})
+
+@login_required
+def profile(request):
+    return render(request,'user/profile.html')
+    
